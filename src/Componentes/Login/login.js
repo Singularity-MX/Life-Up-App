11cf36c2039468364d8ed0277a010da4c7de588f
Jose Javier Gutierrez Ramirez <replacedspace17@gmail.com>
2023-04-22 10:51:24 -0600

    Front de login terminado

--- src/Componentes/Login/login.js
+++ src/Componentes/Login/login.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-import {Header, Loader} from "../Header";
+import { Header, Loader } from "../Header";
 function Login() {
   const [showPassword, setShowPassword] = useState(false);
 
@@ -14,30 +14,26 @@ function Login() {
 
   return (
     <div ><Header texto="Inicia Sesión" />
-    <div className='targetForm'>
-      
-      <div className='panel_izq'></div>
-      <div className='panel_der'>
-        <h1>INICIA SESIÓN</h1>
-        <div className='containerForm'>
-          <form>
-          <FontAwesomeIcon icon={faUser} className="icon" />
+      <div className='targetFormLogin'>
 
-            <input type="text" name="usuario" className="input" placeholder='Nombre de usuario'/><br />
-            
-            <FontAwesomeIcon icon={faLock} className="icon" />
+        <div className='panel_izqLogin'></div>
+        <div className='panel_derLogin'>
+          <h1>INICIA SESIÓN</h1>
+          <div className='containerFormLogin'>
+            <form>
+              <FontAwesomeIcon icon={faUser} className="iconLogin" />
+              <input type="text" name="usuario" className="input-login" placeholder='Nombre de usuario' /><br />
+              <FontAwesomeIcon icon={faLock} className="iconLogin" />
+              <input className="input-login" type={showPassword ? 'text' : 'password'} id="password" name="password" placeholder='Contraseña' /><br />
+              <button type="button" onClick={handleTogglePassword} className='btnShow'>
+                <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} className="ico_show" />{showPassword ? 'Ocultar contraseña' : 'Mostrar contraseña'}
+              </button><br />
 
-            <input className="input" type={showPassword ? 'text' : 'password'} id="password" name="password" placeholder='Contraseña'/><br />
-            
-            <button type="button" onClick={handleTogglePassword} className='btnShow'>
-            <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} className="ico_show"/>{showPassword ? 'Ocultar contraseña' : 'Mostrar contraseña'} 
-            </button><br/>
-
-            <button type="submit" className='boton'>Acceder</button>
-          </form>
+              <button type="submit" className='boton-login'>Acceder</button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
     </div>
 
   );
